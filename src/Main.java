6f87ca68711635821a0fd2a217bfc201cacba1ca
NimZero <26175884+NimZero@users.noreply.github.com>
2021-01-18 18:09:33 +0100

    Added play again prompt and loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         TicTacToe ticTacToe = new TicTacToe();
         if(args.length>0) {
             if(args[0].equalsIgnoreCase("--custom") || args[0].equalsIgnoreCase("-c")) {
@@ -8,6 +11,13 @@ public class Main {
         }else {
             ticTacToe.initBasicPlayers();
         }
-        ticTacToe.play();
+
+        boolean playAgain;
+        do {
+            ticTacToe.play();
+            System.out.println("Play again ? (Y/n)");
+            String again = input.next();
+            playAgain = again.matches("^(?:y(?:es)?|1)$");
+        }while (playAgain);
     }
 }
